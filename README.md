d2eb7fbded0479ea14ba67485ce9db707d819365
juanborre <juanborre@github.aleeas.com>
2025-02-13 09:26:28 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # alfred-timestamp-converter
-Alfred workflow to convert timestamp into string and show how much time ago
+
+Alfred workflow to convert a timestamp in human readable format and how much time ago it was.
+
+Use it by typing `timestamp <VALUE>` in Alfred.
+
+Time is displayed in the local timezone and UTC.
+
+Alternatively, you can select and highlight a timestamp in any application and use the hotkey `Option + t` to convert it.
+
+If you type `Intro` while highlighting a converted string, it will be copied to the clipboard.
+
+![Convert timestamp](./convert-timestamp.jpg)
+
+## Installation
+
+Download the latest version from the [releases page](https://github.com/juanborre/alfred-timestamp-converter/releases)
+and double click the `Timestamp.converter.alfredworkflow` file.
---
+++ convert.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+query=$1
+
+current_time=$(date +%s)
+seconds_ago=$((current_time - $query))
+
+days=$((seconds_ago / 86400))
+remaining_seconds=$((seconds_ago % 86400))
+hours=$((remaining_seconds / 3600))
+remaining_seconds=$((remaining_seconds % 3600))
+minutes=$((remaining_seconds / 60))
+seconds=$((remaining_seconds % 60))
+
+output=""
+
+if [[ $days -gt 0 ]]; then
+output+="${days} days "
+fi
+if [[ $hours -gt 0 ]]; then
+output+="${hours} hours "
+fi
+if [[ $minutes -gt 0 ]]; then
+output+="${minutes} minutes "
+fi
+if [[ $seconds -gt 0 ]]; then
+output+="${seconds} seconds"
+fi
+
+cat << EOB
+{"items": [
+	{
+		"title": "$(date -r $query +"%Y-%m-%d %H:%M:%S %Z")",
+		"arg": "$(date -r $query +"%Y-%m-%d %H:%M:%S %Z")"
+	},
+	{
+		"title": "$(TZ=UTC date -ur $query +"%Y-%m-%dT%H:%M:%S") UTC",
+		"arg": "$(TZ=UTC date -ur $query +"%Y-%m-%dT%H:%M:%S") UTC"
+	},
+	{
+		"title": "$output ago",
+		"arg": "$output ago"
+	}
+]}
+EOB
